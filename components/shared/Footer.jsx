49d5eebe3f7c3c07da2a4b49a4e750002ceca05c
GentlemanMenace <kcrundquist@gmail.com>
2021-01-28 16:43:37 -0600

    added copyright text

--- components/shared/Footer.jsx
+++ components/shared/Footer.jsx
@@ -1,10 +1,13 @@
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function navBar () {
 
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
 
   return (
     <footer>
+      <div className="main-footer">
         <Link href="/">
             <h2 className="footer-logo">LSR Creative</h2>
         </Link>
@@ -16,6 +19,10 @@ export default function navBar () {
                 <i className="fab fa-instagram"></i>
             </a>
         </div>
+      </div>
+      <div className="sub-footer">
+        <small>&copy; Copyright {currentYear}, Logan Rundquist</small>
+      </div>
     </footer>
   );
 }
